78482560b726b678f8e58d07fa9dbf530228ef4c
diego@alpha <diegorodriguezriera@gmail.com>
2018-11-30 16:52:32 +0100

    added part of tests

---
+++ gestion-de-alumnos/gtests/DBControllerUnittest.cpp
@@ -0,0 +1,35 @@
+/* gtests/StudentUnittest.hpp
+ *
+ * Licenced under GPLv3
+ * Authors:
+ *	Diego Rodriguez Riera
+ *
+ * Unittests for Student class
+ *
+ * Not doing mocks becouse of the low complexity of the program
+ */
+ 
+
+#include "gtest/gtest.h"
+#include "DBController.hpp"
+
+TEST(DBController, Builder){
+	std::string path = "/tmp/dbtest.bin";
+	DBController db(path);
+	EXPECT_EQ(db.getPath(), path);
+}
+
+TEST(DBController, loader_empty_and_rm){
+	std::string path = "/tmp/dbtest.bin";
+	DBController db(path);
+	db.rmDB();
+	EXPECT_EQ(db.getDB(), NULL);
+}
+
+TEST(DBController, _setter_and_loader){
+	std::list<Student> students;
+	std::string path = "/tmp/dbtest.bin";
+	DBController db(path);
+	EXPECT_EQ(db.setDB(students), SUCCESS);
+	EXPECT_EQ(db.getDB(), students);
+}	
---
+++ gestion-de-alumnos/src/Controller/DBController.hpp
@@ -0,0 +1,19 @@
+/* src/Controller/DBController.hpp
+ *
+ * Licenced under GPLv3
+ * Authors:
+ *	Diego Rodriguez Riera
+ *
+ * Headers of the sysmtem's DBController
+ */
+
+
+#ifndef __DBCONTROLLER__
+#define __DBCONTROLLER__
+
+class DBController{
+	public:
+	private:
+};
+
+#endif
