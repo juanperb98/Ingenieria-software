ec3a8cbbbfe8aac1f222f9bb61b2c992e5ee35ac
diego@alpha <diegorodriguezriera@gmail.com>
2018-12-11 16:42:05 +0100

    improoved search student

--- gestion-de-alumnos/gtests/ClassroomUnittest.cpp
+++ gestion-de-alumnos/gtests/ClassroomUnittest.cpp
@@ -206,3 +206,69 @@ TEST(Classroom,searchStudent){
 	newClass.addStudent(s4);
 	EXPECT_EQ(newClass.searchStudent(s4,students), SUCCESS);
 }
+
+
+TEST(Classroom, modifyStudent){
+	Classroom classroom;
+	time_t date = time(nullptr);
+
+	Student s1;
+	s1.setId("123");
+	s1.setName("pepe");
+	s1.setLastName("perez");
+	s1.setPhone(689111111);
+	s1.setEmail("pepe@uco.es");
+	s1.setAddress("calle de pepe");
+	s1.setHighestCourse(3);
+	s1.setGroupNumber(4);
+	s1.setBirthAt(date);
+	s1.setIsLeader(true);
+
+
+	Student s2;
+	s2.setId("1234");
+	s2.setName("juan");
+	s2.setLastName("lopez");
+	s2.setPhone(689112111);
+	s2.setEmail("juan@uco.es");
+	s2.setAddress("calle de juan");
+	s2.setHighestCourse(2);
+	s2.setGroupNumber(3);
+	s2.setBirthAt(date);
+	s2.setIsLeader(false);
+
+	Student s3;
+	s3.setId("4321");
+	s3.setName("rocio");
+	s3.setLastName("lopez");
+	s3.setPhone(689112114);
+	s3.setEmail("rocio@uco.es");
+	s3.setAddress("calle de rocio");
+	s3.setHighestCourse(3);
+	s3.setGroupNumber(1);
+	s3.setBirthAt(date);
+	s3.setIsLeader(true);
+
+	Student s4;
+	s4.setName("rocio");
+
+	std::vector<Student> students;
+
+	classroom.addStudent(s1);
+	classroom.addStudent(s2);
+	EXPECT_EQ(classroom.modifyStudent(s2, s3), SUCCESS);
+
+	EXPECT_EQ(classroom.searchStudent(s4, students), SUCCESS);
+	EXPECT_EQ(students.size(), 1);
+
+	std::cout << students[0].getName() << '\n';
+	EXPECT_EQ(students[0].getId(), s3.getId());
+	EXPECT_EQ(students[0].getName(), s3.getName());
+	EXPECT_EQ(students[0].getLastName(), s3.getLastName());
+	EXPECT_EQ(students[0].getPhone(), s3.getPhone());
+	EXPECT_EQ(students[0].getEmail(), s3.getEmail());
+	EXPECT_EQ(students[0].getAddress(), s3.getAddress());
+	EXPECT_EQ(students[0].getHighestCourse(), s2.getHighestCourse());
+	EXPECT_EQ(students[0].getBirthAtUnix(), s3.getBirthAtUnix());
+	EXPECT_EQ(students[0].getIsLeader(), s3.getIsLeader());
+}
--- gestion-de-alumnos/src/Entity/Classroom/Classroom.cpp
+++ gestion-de-alumnos/src/Entity/Classroom/Classroom.cpp
@@ -38,56 +38,135 @@ int Classroom::deleteStudent(Student student){
 }
 
 
+// int Classroom::searchStudent(Student & student,
+// 			     std::vector<Student> &students){
+// 	//Searching for students with progressive filters and some basic regex
+// 	if ( students_.empty() )
+// 		return CLASSROOM_EMPTY;
+// 
+// 	if ( student.getId() ==  ""
+// 	&& student.getName() == ""
+// 	&& student.getLastName() == "" )
+// 		return CLASSROOM_STUDENT_NOT_FOUND;
+// 
+// 	students.clear();
+// 	if ( (student.getId() !=  ""  )){
+// 
+// 		std::vector<Student>::iterator studentIterator;
+// 		for (studentIterator = students_.begin();studentIterator != students_.end();studentIterator++){
+// 			std::string studentId = studentIterator->getId();
+// 			std::string customRegex = ".*("+student.getId()+").*";
+// 			std::regex re(customRegex);
+// 			std::smatch match;
+// 			if (std::regex_search(studentId,match,re))
+// 				students.push_back(*studentIterator);
+// 		}
+// 	}
+// 	if (student.getName() != "" ){
+// 		std::vector<Student> studentsAux;
+// 		std::vector<Student>::iterator studentIterator;
+// 		for (studentIterator=students.begin();studentIterator!=students.end();studentIterator++){
+// 			std::string studentName = studentIterator->getName();
+// 			std::string customRegex = ".*("+student.getName()+").*";
+// 			std::regex re(customRegex);
+// 			std::smatch match;
+// 			if (std::regex_search(studentName,match,re))
+// 				studentsAux.push_back(*studentIterator);
+// 		}
+// 		students = studentsAux;
+// 	}
+// 	if(student.getLastName() != ""){
+// 	std::vector<Student>::iterator studentIterator;
+// 	std::vector<Student> studentsAux;
+// 		for (studentIterator = students.begin();studentIterator!=students.end();studentIterator++){
+// 			std::string regexPattern = studentIterator->getLastName();
+// 			std::string customRegex = ".*("+student.getLastName()+").*";
+// 			std::regex re(customRegex);
+// 			std::smatch match;
+// 			if (std::regex_search(regexPattern,match,re)){
+// 				studentsAux.push_back(*studentIterator);
+// 			}
+// 		}
+// 		students = studentsAux;
+// 	}
+// 
+// 	return SUCCESS;
+// }
+
+
 int Classroom::searchStudent(Student & student,
 			     std::vector<Student> &students){
-	//Searching for students with progressive filters and some basic regex
-	if ( students_.empty() )
+	// loads all the students in the vector
+	students = this->getStudents();
+	
+	//filters out the non matching students
+	std::vector<Student>::iterator studentIterator;
+	std::string regexPattern;
+	
+	if ( students.size() == 0 )
 		return CLASSROOM_EMPTY;
 	
-	if ( student.getId() ==  ""
-	&& student.getName() == ""
-	&& student.getLastName() == "" )
-		return CLASSROOM_STUDENT_NOT_FOUND;
-	
-	if ( (student.getId() !=  ""  )){
-		students.clear();
-		std::vector<Student>::iterator studentIterator;
-		for (studentIterator = students_.begin();studentIterator != students_.end();studentIterator++){
-			std::string studentId = studentIterator->getId();
-			std::string customRegex = ".*("+student.getId()+").*";
-			std::regex re(customRegex);
-			std::smatch match;
-			if (std::regex_search(studentId,match,re))
-				students.push_back(*studentIterator);
-		}
-		if (student.getName() != "" ){
-			std::vector<Student> studentsAux;
-			std::vector<Student>::iterator studentIterator;
-			for (studentIterator=students.begin();studentIterator!=students.end();studentIterator++){
-				std::string studentName = studentIterator->getName();
-				std::string customRegex = ".*("+student.getName()+").*";
-				std::regex re(customRegex);
-				std::smatch match;
-				if (std::regex_search(studentName,match,re))
-					studentsAux.push_back(*studentIterator);
-			}
-			students = studentsAux;
-		}
-		if(student.getLastName() != ""){
-		std::vector<Student>::iterator studentIterator;
-		std::vector<Student> studentsAux;
-			for (studentIterator = students.begin();studentIterator!=students.end();studentIterator++){
-				std::string regexPattern = studentIterator->getLastName();
-				std::string customRegex = ".*("+student.getLastName()+").*";
-				std::regex re(customRegex);
-				std::smatch match;
-				if (std::regex_search(regexPattern,match,re)){
-					studentsAux.push_back(*studentIterator);
-				}
-			}
-			students = studentsAux;
-		}
+	for ( studentIterator = students.begin(); studentIterator != students.end(); studentIterator++ ){
+		
+		//set up the regexs
+		regexPattern = ".*("+student.getId()+").*";
+		std::regex idRegex(regexPattern);
+		regexPattern = ".*("+student.getName()+").*";
+		std::regex nameRegex(regexPattern);
+		regexPattern = ".*("+student.getLastName()+").*";
+		std::regex lastNameRegex(regexPattern);
+		
+		
+		// if field is defined and not match, erase it
+		if ( student.getId() != "" 
+		&& ( !std::regex_match(studentIterator->getId(), idRegex)) )
+			students.erase(studentIterator);
+		
+		if ( student.getName() != "" 
+		&& ( !std::regex_match(studentIterator->getName(), nameRegex)) )
+			students.erase(studentIterator);
+			
+		if ( student.getLastName() != "" 
+		&& ( !std::regex_match(studentIterator->getLastName(), lastNameRegex)) )
+			students.erase(studentIterator);
 	}
+	
+	return SUCCESS;
+}
+
+
+int Classroom::modifyStudent(Student target, Student newData){
+	std::vector<Student> students;
+	this->searchStudent(target, students);
+	
+	// checks for search errors, only one student must be found
+	if ( students.size() == 0 )
+		return CLASSROOM_STUDENT_NOT_FOUND;
+	if ( students.size() > 1 )
+		return CLASSROOM_MULTIPLES_OCURRENCES;
+	
+	Student student = students[0];
+	
+	this->deleteStudent(students[0]);
+	
+	if ( newData.getId() != "" )
+		student.setId(newData.getId());
+	
+	if ( newData.getName() != "" )
+		student.setName(newData.getName());
+	
+	if ( newData.getLastName() != "" )
+		student.setLastName(newData.getLastName());
+	
+	if ( newData.getPhone() != 0 )
+		student.setPhone(newData.getPhone());
+	
+	if ( newData.getEmail() != "" )
+		student.setEmail(newData.getEmail());
+	
+	
+	this->addStudent(student);
+	
 	return SUCCESS;
 }
 
